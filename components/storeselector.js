fa6eb7af68ecf263567706634d24797a23ab77f4
Bryce Eppler <eppler97@gmail.com>
2023-01-17 06:40:02 -0800

    adding checkboxes to multisearch page

--- components/storeselector.js
+++ components/storeselector.js
@@ -1,7 +1,152 @@
 import React from 'react'
+import useStore from '@/store'
+// all websites
+// Aether Vault Games
+// Atlas Collectables
+// Border City Games
+// The Connection Games
+// Everything Games
+// Exor Games
+// Face to Face Games
+// Fantasy Forged Games
+// FirstPlayer
+// 401 Games
+// Fusion Gaming
+// GameKnight
+// Gamezilla
+// Gauntlet Games
+// Hairy Tarantula
+// House of Cards
+// Jeux 3 Dragons
+// Manaforce
+// Magic Stronghold
+// Orchard City Games
+// Sequence Gaming Brockville
+// Topdeck Hero
+// Wizard's Tower (kanatacg)
 
+const websites = [
+    {
+        name: 'Aether Vault Games',
+        code: 'aethervault',
+    },
+    {
+        name: 'Atlas Collectables',
+        code: 'atlas',
+    },
+    {
+        name: 'Border City Games',
+        code: 'bordercity',
+    },
+    {
+        name: 'The Connection Games',
+        code: 'connectiongames',
+    },
+    {
+        name: 'Everything Games',
+        code: 'everythinggames',
+    },
+    {
+        name: 'Exor Games',
+        code: 'exorgames',
+    },
+    {
+        name: 'Face to Face Games',
+        code: 'facetoface',
+    },
+    {
+        name: 'Fantasy Forged Games',
+        code: 'fantasyforged',
+    },
+    {
+        name: 'FirstPlayer',
+        code: 'firstplayer',
+    },
+    {
+        name: '401 Games',
+        code: 'four01',
+    },
+    {
+        name: 'Fusion Gaming',
+        code: 'fusiongaming',
+    },
+    {
+        name: 'GameKnight',
+        code: 'gameknight',
+    },
+    {
+        name: 'Gamezilla',
+        code: 'gamezilla',
+    },
+    {
+        name: 'Gauntlet Games',
+        code: 'gauntlet',
+    },
+    {
+        name: 'Hairy Tarantula',
+        code: 'hairyt',
+    },
+    {
+        name: 'House of Cards',
+        code: 'houseofcards',
+    },
+    {
+        name: 'Jeux 3 Dragons',
+        code: 'jeux3dragons',
+    },
+    {
+        name: 'Manaforce',
+        code: 'manaforce',
+    },
+    {
+        name: 'Magic Stronghold',
+        code: 'magicstronghold',
+    },
+    {
+        name: 'Orchard City Games',
+        code: 'orchardcity',
+    },
+    {
+        name: 'Sequence Gaming Brockville',
+        code: 'sequencegaming',
+    },
+    {
+        name: 'Topdeck Hero',
+        code: 'topdeckhero',
+    },
+    {
+        name: 'Wizard\'s Tower (kanatacg)',
+        code: 'wizardstower',
+    },
+
+]
 export default function StoreSelector() {
+    const { useMultiSearchStore: store  } = useStore()
   return (
-    <div>storeselector</div>
+    <div className="flex flex-col w-full">
+        {
+            websites.map((website, index) => {
+                return (
+                    <div key={index} className="flex items-center">
+                        <input
+                            type="checkbox"
+                            checked={store.getState().websites[website.code]}
+                            onChange={(e) => {
+                                store.setState({
+                                    websites: {
+                                        ...store.getState().websites,
+                                        [website.code]: e.target.checked,
+                                    }
+                                })
+                            }}
+                        />
+                        <label className="ml-2">{website.name}</label>
+                    </div>
+                )
+            }
+            )
+        }
+    </div>
+
   )
 }
--- store.js
+++ store.js
@@ -1,6 +1,100 @@
 import { create } from "zustand";
 
+const websites = [
+  {
+      name: 'Aether Vault Games',
+      code: 'aethervault',
+  },
+  {
+      name: 'Atlas Collectables',
+      code: 'atlas',
+  },
+  {
+      name: 'Border City Games',
+      code: 'bordercity',
+  },
+  {
+      name: 'The Connection Games',
+      code: 'connectiongames',
+  },
+  {
+      name: 'Everything Games',
+      code: 'everythinggames',
+  },
+  {
+      name: 'Exor Games',
+      code: 'exorgames',
+  },
+  {
+      name: 'Face to Face Games',
+      code: 'facetoface',
+  },
+  {
+      name: 'Fantasy Forged Games',
+      code: 'fantasyforged',
+  },
+  {
+      name: 'FirstPlayer',
+      code: 'firstplayer',
+  },
+  {
+      name: '401 Games',
+      code: 'four01',
+  },
+  {
+      name: 'Fusion Gaming',
+      code: 'fusiongaming',
+  },
+  {
+      name: 'GameKnight',
+      code: 'gameknight',
+  },
+  {
+      name: 'Gamezilla',
+      code: 'gamezilla',
+  },
+  {
+      name: 'Gauntlet Games',
+      code: 'gauntlet',
+  },
+  {
+      name: 'Hairy Tarantula',
+      code: 'hairyt',
+  },
+  {
+      name: 'House of Cards',
+      code: 'houseofcards',
+  },
+  {
+      name: 'Jeux 3 Dragons',
+      code: 'jeux3dragons',
+  },
+  {
+      name: 'Manaforce',
+      code: 'manaforce',
+  },
+  {
+      name: 'Magic Stronghold',
+      code: 'magicstronghold',
+  },
+  {
+      name: 'Orchard City Games',
+      code: 'orchardcity',
+  },
+  {
+      name: 'Sequence Gaming Brockville',
+      code: 'sequencegaming',
+  },
+  {
+      name: 'Topdeck Hero',
+      code: 'topdeckhero',
+  },
+  {
+      name: 'Wizard\'s Tower (kanatacg)',
+      code: 'wizardstower',
+  },
 
+]
 
 const homePageStore = (set) => ({
   resultsRaw: [],
@@ -48,9 +142,29 @@ const multiSearchStore = (set) => ({
   cardNames: [],
   setCardNames: (cardNames) => set({ cardNames }),
   websites: {
+    aethervault: true,
+    atlas: true,
+    bordercity: true,
+    connectiongames: true,
+    everythinggames: true,
+    exorgames: true,
+    facetoface: true,
+    fantasyforged: true,
+    firstplayer: true,
     four01: true,
+    fusiongaming: true,
+    gameknight: true,
+    gamezilla: true,
     gauntlet: true,
+    hairyt: true,
+    houseofcards: true,
+    jeux3dragons: true,
+    manaforce: true,
+    magicstronghold: true,
+    orchardcity: true,
+    sequencegaming: true,
     topdeckhero: true,
+    wizardstower: true, 
   },
   setWebsites: (websites) => set({ websites }),
 });
