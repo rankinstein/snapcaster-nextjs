9bfff834006f1c8aa667c9aeadfa96917cb1a99e
Bryce Eppler <eppler97@gmail.com>
2023-01-19 17:03:33 -0800

    gradient on header

--- components/homebanner.js
+++ components/homebanner.js
@@ -4,9 +4,9 @@ export default function Homebanner() {
   return (
     <div className="pt-24">
       {" "}
-      <h1 className="text-6xl font-bold text-purple-600">
+      <div className="text-6xl font-bold bg-clip-text bg-gradient-to-r from-purple-600 to-purple-700 text-transparent">
         <a href="https://snapcaster.ca">snapcaster</a>
-      </h1>
+      </div>
       <p className="mt-5 text-2xl">Get started by searching for a card </p>
     </div>
   );
