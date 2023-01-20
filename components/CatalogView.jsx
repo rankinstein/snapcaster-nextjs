5b654c1b5436df684b5ec31ff2e4582f298ab7af
Bryce Eppler <eppler97@gmail.com>
2023-01-20 09:52:07 -0800

    back to top button on single search

--- components/CatalogView.jsx
+++ components/CatalogView.jsx
@@ -7,6 +7,17 @@ export default function CatalogView({ cardData }) {
       {cardData.map((cardData, index) => (
         <SearchRow cardData={cardData} key={index} />
       ))}
+      {/* if cardData is longer than 10, have a back to top button */}
+      {cardData.length > 10 && (
+        <button
+          onClick={() => {
+            window.scrollTo(0, 0);
+          }}
+          className="btn-small mt-4"
+        >
+          Back to Top
+        </button>
+      )}
     </div>
   );
 }
