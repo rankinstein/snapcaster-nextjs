e50fac932cd98c525e65da02d01c40cf242a332b
Bryce Eppler <eppler97@gmail.com>
2023-01-22 05:41:20 -0800

    card image added

--- components/linechart.js
+++ components/linechart.js
@@ -2,16 +2,9 @@ import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
 
 const LineChart = ({ data }) => {
-  const data2 = [
-    { date: new Date("2022-01-01"), price: 100 },
-    { date: new Date("2022-02-01"), price: 95 },
-    { date: new Date("2022-03-01"), price: 99 },
-    { date: new Date("2022-04-01"), price: 90 },
-    { date: new Date("2022-05-01"), price: 100 },
-    { date: new Date("2022-06-01"), price: 120 },
-    { date: new Date("2022-07-01"), price: 110 },
-  ];
-
+  const avgData = data.price_data.map(d => {
+    return { date: new Date(d.date), price: d.avg_price }
+});
   const svgRef = useRef(null);
   const width = 500;
   const height = 300;
@@ -21,12 +14,12 @@ const LineChart = ({ data }) => {
 
     const xScale = d3
       .scaleTime()
-      .domain(d3.extent(data, (d) => d.date))
+      .domain(d3.extent(avgData, (d) => d.date))
       .range([0, width]);
 
     const yScale = d3
       .scaleLinear()
-      .domain([0, d3.max(data, (d) => d.price)])
+      .domain([0, d3.max(avgData, (d) => d.price)])
       .range([height, 0]);
 
     const lineGenerator = d3
@@ -43,11 +36,11 @@ const LineChart = ({ data }) => {
       
     svg
       .selectAll(".line")
-      .data(data)
+      .data(avgData)
       .enter()
       .append("path")
       .attr("class", "line")
-      .attr("d", lineGenerator(data))
+      .attr("d", lineGenerator(avgData))
       .attr("stroke", "purple")
       .on("mouseover", (d, event) => {
         if (svgRef.current) {
@@ -59,32 +52,10 @@ const LineChart = ({ data }) => {
           .style("top", `${event.pageY - svgBounds.top - 28}px`);
         }
       })
-    
-
       .on("mouseout", (d) => {
         tooltip.transition().duration(500).style("opacity", 0);
       });
-
-    svg
-      .selectAll(".line2")
-      .data(data2)
-      .enter()
-      .append("path")
-      .attr("class", "line2")
-      .attr("d", lineGenerator(data2))
-      .attr("stroke", "lightblue")
-      .on("mouseover", (d, event) => {
-        if (svgRef.current) {
-        const svgBounds = svgRef.current.getBoundingClientRect();
-        tooltip.transition().duration(200).style("opacity", 0.9);
-        tooltip
-          .html(`Price: ${d.price}`)
-          .style("left", `${event.pageX - svgBounds.left}px`)
-          .style("top", `${event.pageY - svgBounds.top - 28}px`);
-        }
-      })
     
-
     svg
       .append("g")
       .call(d3.axisBottom(xScale))
--- pages/stocks/[card].js
+++ pages/stocks/[card].js
@@ -1,29 +1,17 @@
 import LineChart from "../../components/linechart";
 
 export default function Card({ card, data }) {
-  const dummyData = [
-    { date: new Date("2022-01-01"), price: 100 },
-    { date: new Date("2022-02-01"), price: 170 },
-    { date: new Date("2022-03-01"), price: 120 },
-    { date: new Date("2022-04-01"), price: 130 },
-    { date: new Date("2022-05-01"), price: 110 },
-    { date: new Date("2022-06-01"), price: 150 },
-    { date: new Date("2022-07-01"), price: 160 },
-    { date: new Date("2022-08-01"), price: 140 },
-    // ...
-  ];
-  const graphData = data.price_data.map(d => {
-    return { date: new Date(d.date), price: d.avg_price }
-});
-console.log(graphData)
+
   // hit pricedata api for the card
   return (
     <div>
       <div className="flex flex-row justify-center items-center">
         {/* image of magic card placeholder */}
-        <div className="ml-8 mr-8 p-2 w-36 h-48 bg-gray-400"></div>
+       <img src={data.image_uri} alt={data.card_name} 
+        className="w-48 h-auto p-4"
+       /> 
         
-        <LineChart data={graphData} />
+        <LineChart data={data}/>
       </div>
       <h1>
         {data.price_data.map((item, index) => (
