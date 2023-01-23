407899fd72fa45dee02ee14fd2b3ef1b50fcafd5
Bryce Eppler <eppler97@gmail.com>
2023-01-22 21:16:59 -0800

    chart done, just need to center content on the page

--- components/rechartsline.js
+++ components/rechartsline.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { useEffect } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const RechartsLineChart = ({price_data}) => {
   const [data, setData] = React.useState([])
   useEffect(() => {
     setData(price_data)
     }, [price_data])
+  
+    
   return (
-    <div className="p-4 bg-gray-800 rounded-md">
+    <div className="p-4 dark:bg-gray-800 rounded-md">
+      <ResponsiveContainer width="100%" height={300}>
     <LineChart width={600} height={300} data={price_data}>
       <XAxis dataKey="date" />
       <YAxis />
@@ -19,6 +22,7 @@ const RechartsLineChart = ({price_data}) => {
       <Line type="monotone" dataKey="avg_price" stroke="#d344ff" activeDot={{ r: 6 }} strokeWidth={3} />
       <Line type="monotone" dataKey="min_price" stroke="red" strokeWidth={3} />
     </LineChart>
+    </ResponsiveContainer>
     </div>
   );
 }
--- pages/stocks/[card].js
+++ pages/stocks/[card].js
@@ -1,24 +1,54 @@
 import RechartsLineChart from "../../components/rechartsline";
 export default function Card({ card, data }) {
   // hit pricedata api for the card
-  console.log(data.price_data[data.price_data.length - 1].min_price)
+  console.log(data.price_data[data.price_data.length - 1].min_price);
   return (
-    <div>
-      <div className="flex flex-row justify-center items-center">
-        <div className="flex flex-col justify-center items-center">
-          {/* image of magic card placeholder */}
-          <img
-            src={data.image_uri}
-            alt={data.card_name}
-            className="w-48 h-auto m-4 rounded-lg"
-          />
-          <h1 className="text-xl font-bold">{data.card_name}</h1>
-          <div
-            className="transition-all bg-purple-600 hover:bg-purple-700 text-white text-sm rounded-lg p-2 m-2"
-          >Purchase from ${data.price_data[data.price_data.length - 1].min_price} CAD</div>
+    <div className="flex flex-col justify-center items-center p-8 min-h-screen">
+      {/* Card image and price chart */}
+      <div className="grid grid-cols-12 w-full">
+        <div className="col-span-12 lg:col-span-4">
+          <div className="flex flex-col justify-center items-center">
+            {/* image of magic card placeholder */}
+            <img
+              src={data.image_uri}
+              alt={data.card_name}
+              className="w-48 h-auto rounded-lg"
+            />
+            <h1 className="text-lg font-bold">{data.card_name}</h1>
+            <div className="transition-all bg-purple-600 hover:bg-purple-700 text-white text-sm rounded-lg p-2 m-2">
+              From $
+              {data.price_data[data.price_data.length - 1].min_price} CAD
+            </div>
+          </div>
         </div>
+        <div className="col-span-12 lg:col-span-8">
+          <RechartsLineChart price_data={data.price_data} />
+        </div>
+      </div>
 
-        <RechartsLineChart price_data={data.price_data} />
+      {/* Table displaying the data entries */}
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-xl font-bold">Price Data</h1>
+        <table className="table-auto">
+          <thead>
+            <tr>
+              <th className="px-4 py-2">Date</th>
+              <th className="px-4 py-2">Avg Price</th>
+              <th className="px-4 py-2">Max Price</th>
+              <th className="px-4 py-2">Min Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.price_data.map((element) => (
+              <tr>
+                <td className="border px-4 py-2">{element.date}</td>
+                <td className="border px-4 py-2">{element.avg_price}</td>
+                <td className="border px-4 py-2">{element.max_price}</td>
+                <td className="border px-4 py-2">{element.min_price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
@@ -30,7 +60,7 @@ export async function getServerSideProps(context) {
 
   let data = null;
   await fetch(`http://localhost:8000/pricedata/card/${card}/`)
-  // await fetch(`https://snapcasterv2-api-production.up.railway.app/pricedata/card/${card}/`)
+    // await fetch(`https://snapcasterv2-api-production.up.railway.app/pricedata/card/${card}/`)
     .then((res) => res.json())
     .then((json) => {
       // before setting data=json, we want to round the prices to 2 decimal places
@@ -42,7 +72,6 @@ export async function getServerSideProps(context) {
 
       data = json;
     });
-
 
   return {
     props: {
