3705f513646ec0e5da2334562634adb3ab3f1d09
Bryce Eppler <eppler97@gmail.com>
2023-01-23 07:29:53 -0800

    next link fix on navbar

---
+++ components/stocksearchbox.js
@@ -0,0 +1,111 @@
+import axios from "axios";
+import useStore from "@/store";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+export default function Stocksearchbox() {
+    /*
+    This is the searchbox component for the stocks page. It is a modified version of the searchbox component from the homepage.
+    It also uses the homePageStore since it's not necessary to create a new store for this. All the content on [card].js is SSR.
+    The only difference is the handleSubmit function, which redirects to the stocks/[cardname] page instead of the homepage.
+    Searhboxes should be refactored to be reusable components.
+    */
+    const router = useRouter();
+  const { useHomePageStore } = useStore();
+  const { 
+    searchQuery, 
+    setSearchQuery,
+    setOnlySearchQuery, 
+    setSearchedQuery, 
+    loading, 
+    setLoading, 
+    setResultsRaw, 
+    setResults, 
+    setShowBanner,
+    showAutoComplete,
+    setShowAutoComplete,
+    autoCompleteResults,
+  } = useHomePageStore();
+
+  useEffect(() => {
+    if (!loading) {
+      return
+    }
+
+    // navigate to the stocks/[cardname] page
+    router.push(`/stocks/${searchQuery}`);
+    
+
+  }, [loading, searchQuery, setLoading, setResults, setResultsRaw, setSearchedQuery, setShowBanner])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setShowAutoComplete(false);
+    setLoading(true);
+  };
+
+  return (
+    <div className="mt-6 w-full">
+      <div className="relative">
+        <form
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          <input
+            type="text"
+            className="block w-full px-4 py-2  placeholder-gray-500 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-purple-500 focus:border-purple-500 sm:text-sm"
+            placeholder="Search"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            spellCheck="false"
+          />
+          <div
+            className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
+            onClick={(e) => {
+              handleSubmit(e);
+            }}
+          >
+            <svg
+              width="14"
+              height="14"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M18.319 14.4326C20.7628 11.2941 20.542 6.75347 17.6569 3.86829C14.5327 0.744098 9.46734 0.744098 6.34315 3.86829C3.21895 6.99249 3.21895 12.0578 6.34315 15.182C9.22833 18.0672 13.769 18.2879 16.9075 15.8442C16.921 15.8595 16.9351 15.8745 16.9497 15.8891L21.1924 20.1317C21.5829 20.5223 22.2161 20.5223 22.6066 20.1317C22.9971 19.7412 22.9971 19.1081 22.6066 18.7175L18.364 14.4749C18.3493 14.4603 18.3343 14.4462 18.319 14.4326ZM16.2426 5.28251C18.5858 7.62565 18.5858 11.4246 16.2426 13.7678C13.8995 16.1109 10.1005 16.1109 7.75736 13.7678C5.41421 11.4246 5.41421 7.62565 7.75736 5.28251C10.1005 2.93936 13.8995 2.93936 16.2426 5.28251Z"
+                fill="currentColor"
+              />
+            </svg>
+          </div>
+        </form>
+        {/* Autocomplete results from store.autoCompleteResults */}
+        {/* when clicking anywhere outside the autocomplete, set showAutoComplete to false */}
+        {showAutoComplete && (
+          <div className="absolute top-12 w-full bg-gray-800 rounded-md shadow-lg"
+            onClick={() => {
+              setShowAutoComplete(false);
+            }}
+          >
+            <ul className="divide-y divide-gray-700">
+              {autoCompleteResults.map((result) => (
+                <li
+                  key={result}
+                  className="px-3 py-3 cursor-pointer hover:bg-gray-700"
+                  onClick={(e) => {
+                    setOnlySearchQuery(result);
+                    handleSubmit(e);
+                  }}
+                >
+                  <p className="text-sm text-gray-300">{result}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
--- pages/stocks/index.js
+++ pages/stocks/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
+import Stocksearchbox from '@/components/stocksearchbox'
+import useStore from '@/store'
+import Loadingspinner from '@/components/loadingspinner'
 
 export default function Stocks({ test }) {
   const [searchQuery , setSearchQuery] = React.useState('')
-  const handleClick = () => {
-    // open a new tab with /stocks/[cardname]
-    window.open(`/stocks/${searchQuery}/`, '_blank')
-    
-  }
+  const { useHomePageStore } = useStore();
+  const { loading } = useHomePageStore();
   return (
     <>
     <Head>
@@ -21,25 +21,19 @@ export default function Stocks({ test }) {
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <main className="flex flex-col justify-between items-center p-8 min-h-screen">
-      <div className="flex-col items-center justify-center flex-1 text-left max-w-xl">
+      <div className="flex-col items-center justify-center flex-1 text-left max-w-xl w-full">
         <div className="mx-auto space-y-7">
           <div>
             <h1 className="text-3xl font-bold text-purple-400 text-center">Stocks</h1>
             {/* search box to search for a card name */}
-            <div className="flex justify-center">
-              <input
-                type="text"
-                className="px-4 py-2 border border-gray-300"
-                placeholder="Search for a card"
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)}
-              />
-              <button
-                className="px-4 py-2 bg-purple-400 text-white"
-                onClick={handleClick}
-              >
-                Search
-              </button>
+            <div className="flex flex-col justify-center">
+            <Stocksearchbox />
+            {loading && (
+              <div className="flex justify-center items-center pt-5">
+                <Loadingspinner />
+              </div>
+            )}
+            
 
               
               </div>
